8a2ad9b54ee0e31491b6887e1e17605830c430bd
SeungHan <jk05018@naver.com>
2022-01-03 20:34:03 +0900

    kadane algorithm

---
+++ src/com/company/leetcode/sort/KadaneAlgorithm.java
@@ -0,0 +1,17 @@
+package com.company.leetcode.sort;
+
+// Kadane's Algorithm
+class KadaneAlgorithm {
+	public int maxSubArray(int[] nums) {
+		int sum = 0, max = Integer.MIN_VALUE;
+		for (int i = 0; i < nums.length; ++i) {
+			sum += nums[i];
+			max = Math.max(max, sum);
+
+			if (sum < 0)
+				sum = 0;
+		}
+
+		return max;
+	}
+}
