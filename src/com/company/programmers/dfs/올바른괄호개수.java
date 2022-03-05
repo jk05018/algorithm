1e9072106063e0bc8bac71c8ef7c10406c7a222e
SeungHan <jk05018@naver.com>
2022-03-05 22:27:22 +0900

    커뮤러닝 dfs, dp

---
+++ src/com/company/programmers/dfs/올바른괄호개수.java
@@ -0,0 +1,25 @@
+package com.company.programmers.dfs;
+
+public class 올바른괄호개수 {
+	public int solution(int n) {
+		return backtracking(n, n);
+	}
+
+	// using backtracking
+	// l_cnt : ( 개수
+	// r_cnt : ) 개수
+	public int backtracking(int l_cnt, int r_cnt) {
+		if (l_cnt < 0 || r_cnt < 0)
+			return 0;
+		if (l_cnt < r_cnt)
+			return 0; // 지금까지 ( 보다 )가 더 많다면 return 0;
+		if (l_cnt == 0 && r_cnt == 0)
+			return 1; //()를 전부 다 사용했다면
+
+		int ret = 0;
+		ret += backtracking(l_cnt - 1, r_cnt); // ( 사용
+		ret += backtracking(l_cnt, r_cnt - 1); // ) 사용
+
+		return ret;
+	}
+}
---
+++ src/com/company/programmers/dp/정수삼각형.java
@@ -0,0 +1,32 @@
+package com.company.programmers.dp;
+
+import java.util.Arrays;
+
+public class 정수삼각형 {
+	static int N;
+	static int[][] cache, nums; // 깔끔한 코드를 위한 전역변수 사용
+
+	// using dynamic programming
+	public int solution(int[][] triangle) {
+		nums = triangle;
+		N = triangle.length;
+		// cache 초기화
+		cache = new int[N][N];
+		for (int[] arr : cache) {
+			Arrays.fill(arr, -1);
+		}
+
+		return path(0, 0);
+	}
+
+	public int path(int x, int y) {
+		if (x >= N || y >= N)
+			return 0;
+		if (x == N - 1)
+			return nums[x][y];
+		if (cache[x][y] != -1)
+			return cache[x][y];
+
+		return cache[x][y] = Math.max(path(x + 1, y), path(x + 1, y + 1)) + nums[x][y];
+	}
+}
